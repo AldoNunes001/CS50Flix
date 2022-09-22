b77f7ea30c7ae9144e688092da2f4d1dd0809e0d
Aldo Nunes <aldonunes001@gmail.com>
2022-09-22 17:29:32 -0300

    testing views by function based views (FBV)

--- course/urls.py
+++ course/urls.py
@@ -2,8 +2,9 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from .views import homepage
+from .views import homepage, homecourses
 
 urlpatterns = [
     path('', homepage),
+    path('courses', homecourses),
 ]
--- course/views.py
+++ course/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import Course
 
 
 # Create your views here.
-
 def homepage(request):
     return render(request, "homepage.html")
+
+
+def homecourses(request):
+    context = {}
+    course_list = Course.objects.all()
+    context['course_list'] = course_list
+    return render(request, "homecourses.html", context)
