2a83c0ffeb361a5f477e13f23829efa15c82b70d
Aldo Nunes <aldonunes001@gmail.com>
2022-09-26 22:53:11 -0300

    Editing homecourses page

--- course/new_context.py
+++ course/new_context.py
@@ -1,12 +1,12 @@
 from .models import Course
 
 def recent_courses(request):
-    lst_recent_courses = Course.objects.all().order_by('-creation_date')[0:10]
+    lst_recent_courses = Course.objects.all().order_by('-creation_date')[0:8]
 
     return {"recent_courses": lst_recent_courses}
 
 
 def popular_courses(request):
-    lst_popular_courses = Course.objects.all().order_by('-views')[0:10]
+    lst_popular_courses = Course.objects.all().order_by('-views')[0:8]
 
     return {"popular_courses": lst_popular_courses}
